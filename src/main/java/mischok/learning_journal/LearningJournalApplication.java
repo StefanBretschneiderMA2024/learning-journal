8e14b932123617fd1fcd4832b6e13f4c8a9e0ccf
stefan <marlowsoprano1234@gmail.com>
2024-09-18 09:30:11 +0200

    first commit

---
+++ src/main/java/mischok/learning_journal/LearningJournalApplication.java
@@ -0,0 +1,13 @@
+package mischok.learning_journal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningJournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningJournalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mischok/learning_journal/LearningJournalApplicationTests.java
@@ -0,0 +1,13 @@
+package mischok.learning_journal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningJournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
