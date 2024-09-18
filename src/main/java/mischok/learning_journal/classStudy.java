590472940d9ef93859998a97c0f1a490e0aec448
Mirko <schulz_mirko@web.de>
2024-09-18 10:53:17 +0200

    created topic and classStudy class, name change due to Intellij

---
+++ src/main/java/mischok/learning_journal/classStudy.java
@@ -0,0 +1,25 @@
+package mischok.learning_journal;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class classStudy {
+    int classID;
+    String name;
+
+    public classStudy(String name){
+
+    }
+
+    public void enterClass(){
+
+    }
+
+    public void leaveClass(){
+
+    }
+
+}
+
---
+++ src/main/java/mischok/learning_journal/topic.java
@@ -0,0 +1,13 @@
+package mischok.learning_journal;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class topic {
+    int topicID;
+    String name;
+    int classID;
+
+}
